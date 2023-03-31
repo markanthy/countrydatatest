8e6ddd9a8cc27eb718e4d30a189f664cd7dba96d
markanthy <4mark.simms@gmail.com>
2023-03-31 17:25:07 -0400

    Added section title div

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <div className={styles.sectiontitle}>Dat geography doe</div>
         <div>
           {data && (
             <>
